8ac96e7e711c935ef1a9629920c381097b6a92b0
Epicki128 <epicki128@proton.me>
2025-11-11 22:46:58 +0100

    Dodaj pliki projektów.

---
+++ OperacjeTekstowe/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ZadaniaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string text;
+            Console.WriteLine("Podaj tekst:");
+            text = Console.ReadLine();
+            int vovelCount = EditText.countVovels(text);
+            Console.WriteLine($"Liczba samogłosek: {vovelCount}");
+            string editedText = EditText.removeRepeatedChars(text);
+            Console.WriteLine($"Tekst po usunięciu powtórzonych znaków: \"{editedText}\"");
+        }
+    }
+
+    class EditText
+    {
+
+        static public int countVovels(string text)
+        {
+            int count = 0;
+            string vowels = "aąeęioóuAĄEĘIOÓU";
+            foreach (char c in text)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static public string removeRepeatedChars(string text)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (result.EndsWith(c))
+                {
+                    continue;
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+    }
+}

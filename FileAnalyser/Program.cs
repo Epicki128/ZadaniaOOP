5a1acd38d9e418e46e67fde4d8b857699928bb9d
Epicki128 <epicki128@proton.me>
2026-04-02 00:08:24 +0200

    Finished Assignment "File Analyser" & "Save to File"

---
+++ FileAnalyser/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace FileAnalyser
+{
+    internal class Program
+    {
+        static readonly char[] Vowels = { 'a', 'e', 'i', 'y', 'o', 'u', 'A', 'E', 'I', 'O', 'Y', 'U' };
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string fileName;
+
+                if (args.Length >= 1)
+                    fileName = args[0];
+                else
+                {
+                    Console.Write("Podaj sciezke do pliku: ");
+                    fileName = Console.ReadLine() ?? "";
+                }
+
+                if (string.IsNullOrEmpty(fileName))
+                    throw new Exception("File name was not provided!");
+
+                int charsCount = 0;
+                int linesCount = 0;
+                int vowelsCount = 0;
+                int consonantsCount = 0;
+                int wordsCount = 0;
+
+                string[] lines = File.ReadAllLines(fileName);
+
+                foreach (string line in lines)
+                {
+                    linesCount++;
+
+                    string[] words = line
+                        .Split(' ')
+                        .Where(w => w != "\n" && w != "")
+                        .ToArray();
+
+                    wordsCount += words.Length;
+
+                    foreach (char ch in line)
+                    {
+                        if (ch != '\n')
+                            charsCount++;
+
+                        if (Vowels.Contains(ch))
+                            vowelsCount++;
+                        else if (!char.IsWhiteSpace(ch))
+                            consonantsCount++;
+                    }
+                }
+
+                long fileSize = new FileInfo(fileName).Length;
+
+                Console.WriteLine($"size of file: {fileSize} bytes");
+                Console.WriteLine($"count of words: {wordsCount}");
+                Console.WriteLine($"count of lines: {linesCount}");
+                Console.WriteLine($"count of characters (with spaces): {charsCount}");
+
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Without whitespaces!");
+                Console.WriteLine("------------------------");
+
+                Console.WriteLine($"count of vowels: {vowelsCount}");
+                Console.WriteLine($"count of consonants: {consonantsCount}");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("[ERROR]: File not found!");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("[ERROR]: File not exist!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERROR]: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SaveToFile/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace SaveToFile
+{
+    internal class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Club { get; set; }
+
+        public Player(int id, string name, int age, string club)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Club = club;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Player> players = new List<Player>
+            {
+                new Player(1, "Paulo",    20, "FC Potato"),
+                new Player(2, "Karl",     30, "FC Tomato"),
+                new Player(3, "Grzegorz", 25, "FC Polako"),
+                new Player(4, "John",     24, "FC Uralto"),
+                new Player(5, "Fabiano",  22, "FC Utarto"),
+            };
+
+                string fileName;
+
+                if (args.Length < 1)
+                {
+                    Console.Write("Podaj nazwe pliku do ktorego chcesz zapisac dane (z .txt): ");
+                    fileName = Console.ReadLine() ?? "";
+                }
+                else
+                {
+                    fileName = args[0];
+                }
+
+                using StreamWriter writer = new StreamWriter(fileName);
+
+                foreach (Player player in players)
+                {
+                    writer.WriteLine($"Gracz o ID: {player.Id}");
+                    writer.WriteLine(player.Name);
+                    writer.WriteLine(player.Age);
+                    writer.WriteLine(player.Club);
+                    writer.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
